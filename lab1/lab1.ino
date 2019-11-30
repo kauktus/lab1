e494ee43b72aaa55f29c9c46c3233f1dbf12e209
kauktus <kauktus@yandex.ru>
2019-11-30 15:55:34 +0500

    delete comments

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -4,23 +4,14 @@
 #define PIN_BUZZER 6
 Buzzer buzzer(PIN_BUZZER);
 
-
-//String notes[] = {"A", "AS", "B", "C", "CS", "D", "DS", "E", "F", "FS", "G", "GS"}; 
 int noteName = 0;
 double duration = 8;
 int melodyLength = 2;
 int startTime = 0;
-//int note = Serial.parseInt();
-//double durations[] = {8, 8, 1, 8, 1, 8, 1, 24};
-//int melodyLength = 8;
 
 void setup() {
-//    buzzer.setMelody(note, durations, melodyLength);
-//    buzzer.turnSoundOn();
     Serial.begin(9600);
     buzzer.play(noteName, duration, melodyLength);
-    //buzzer = new Buzzer(PIN_BUZZER);
-    //Serial.parseInt();
 }
 
 void loop()
